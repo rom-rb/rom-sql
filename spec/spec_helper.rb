5816dbcd901afcc9a37d02cc6ac85d3697980f22
Piotr Solnica <piotr.solnica@gmail.com>
2014-11-12 21:03:16 +0100

    Move spec support to lib

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,10 @@ require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
 require 'rom-sql'
+require 'rom-sql/spec/support'
 
 root = Pathname(__FILE__).dirname
 
-Dir[root.join('shared/*.rb').to_s].each { |f| puts f; require f }
+Dir[root.join('shared/*.rb').to_s].each { |f| require f }
 
 require root.join('support/db')
