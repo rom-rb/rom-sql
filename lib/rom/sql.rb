7f5b71c58b259becf2c278090995cc55f8f69b2e
Piotr Solnica <piotr.solnica@gmail.com>
2014-11-22 21:18:23 +0100

    Extend dataset with rename and prefix ops

--- lib/rom/sql.rb
+++ lib/rom/sql.rb
@@ -2,6 +2,21 @@ require "sequel"
 
 class Sequel::Dataset
   alias_method :header, :columns
+
+  def rename(options)
+    columns = header - options.keys
+    options.each { |old, new| columns << :"#{name}__#{old}___#{new}" }
+
+    select(*columns)
+  end
+
+  def prefix(col_prefix)
+    rename(Hash[header.map { |col| [col, "#{col_prefix}_#{col}"] }])
+  end
+
+  def name
+    opts[:from].first
+  end
 end
 
 require "rom"
