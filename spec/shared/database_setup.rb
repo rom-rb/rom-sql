cb6d5ba6ba616902e69e0c626f512405af382818
Piotr Solnica <piotr.solnica@gmail.com>
2014-11-28 22:40:57 +0100

    Fix db setup context to create the actual db tables

--- spec/shared/database_setup.rb
+++ spec/shared/database_setup.rb
@@ -6,5 +6,30 @@ shared_context 'database setup' do
 
   before do
     setup.postgres.use_logger(LOGGER)
+
+    [:users, :tasks, :tags, :task_tags].each { |name| conn.drop_table?(name) }
+
+    conn.create_table :users do
+      primary_key :id
+      String :name
+    end
+
+    conn.create_table :tasks do
+      primary_key :id
+      Integer :user_id
+      String :title
+    end
+
+    conn.create_table :tags do
+      primary_key :id
+      String :name
+    end
+
+    conn.create_table :task_tags do
+      primary_key :tag_id, :task_id
+      Integer :tag_id
+      Integer :task_id
+    end
+
   end
 end
--- spec/shared/users_and_tasks.rb
+++ spec/shared/users_and_tasks.rb
@@ -2,30 +2,6 @@ shared_context 'users and tasks' do
   include_context 'database setup'
 
   before do
-    [:users, :tasks, :tags, :task_tags].each { |name| conn.drop_table?(name) }
-
-    conn.create_table :users do
-      primary_key :id
-      String :name
-    end
-
-    conn.create_table :tasks do
-      primary_key :id
-      Integer :user_id
-      String :title
-    end
-
-    conn.create_table :tags do
-      primary_key :id
-      String :name
-    end
-
-    conn.create_table :task_tags do
-      primary_key :tag_id, :task_id
-      Integer :tag_id
-      Integer :task_id
-    end
-
     conn[:users].insert id: 1, name: 'Piotr'
     conn[:tasks].insert id: 1, user_id: 1, title: 'Finish ROM'
     conn[:tags].insert id: 1, name: 'important'
