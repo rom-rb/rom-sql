ca9c68d0f0b4fdcd1194ba724b973f3f3648d52a
Piotr Solnica <piotr.solnica@gmail.com>
2014-12-06 15:51:02 +0100

    Remove redundant attr reader

--- lib/rom/sql/adapter.rb
+++ lib/rom/sql/adapter.rb
@@ -6,7 +6,7 @@ module ROM
   module SQL
 
     class Adapter < ROM::Adapter
-      attr_reader :connection, :logger
+      attr_reader :logger
 
       def self.schemes
         [:ado, :amalgalite, :cubrid, :db2, :dbi, :do, :fdbsql, :firebird, :ibmdb,
