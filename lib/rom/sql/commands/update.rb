1d92bd869926af5eebb9f4c6c7f8c67909839bcd
Piotr Solnica <piotr.solnica@gmail.com>
2015-02-04 16:43:16 +0100

    Add `update(:rel_name).change(original).to(new_one)` interface
    refs rom-rb/rom#115

--- lib/rom/sql/commands/update.rb
+++ lib/rom/sql/commands/update.rb
@@ -6,15 +6,39 @@ module ROM
       class Update < ROM::Commands::Update
         include TupleCount
 
+        option :original, type: Hash, reader: true
+
+        alias_method :to, :call
+
         def execute(tuple)
           attributes = input[tuple]
           validator.call(attributes)
 
-          pks = relation.map { |t| t[relation.model.primary_key] }
+          changed = diff(attributes.to_h)
 
-          relation.update(attributes.to_h)
-          relation.unfiltered.where(relation.model.primary_key => pks)
+          if changed.any?
+            pks = relation.map { |t| t[relation.model.primary_key] }
+            relation.update(changed)
+            relation.unfiltered.where(relation.model.primary_key => pks)
+          else
+            []
+          end
         end
+
+        def change(original)
+          self.class.new(relation, options.merge(original: original))
+        end
+
+        private
+
+        def diff(tuple)
+          if original
+            Hash[tuple.to_a - (tuple.to_a & original.to_a)]
+          else
+            tuple
+          end
+        end
+
       end
     end
   end
--- spec/integration/commands/update_spec.rb
+++ spec/integration/commands/update_spec.rb
@@ -1,26 +1,50 @@
 require 'spec_helper'
 
 describe 'Commands / Update' do
-  include_context 'users and tasks'
+  include_context 'database setup'
 
   subject(:users) { rom.commands.users }
 
-  it 'works' do
+  let(:relation) { rom.relations.users }
+  let(:piotr) { relation.by_name('Piotr').first }
+  let(:peter) { { name: 'Peter' } }
+
+  before do
     setup.relation(:users) do
+      def by_id(id)
+        where(id: id).limit(1)
+      end
+
       def by_name(name)
         where(name: name)
       end
     end
 
     setup.commands(:users) do
-      define(:update) do
-        input Hash
-        validator proc {}
-      end
+      define(:update)
     end
 
-    result = users.try { update(:by_name, 'Piotr').set(name: 'Peter') }
+    relation.insert(name: 'Piotr')
+  end
+
+  it 'updates when attributes changed' do
+    result = users.try do |c|
+      c.update(:by_id, piotr[:id]).change(piotr).to(peter)
+    end
 
     expect(result.value.to_a).to match_array([{ id: 1, name: 'Peter' }])
+  end
+
+  it 'does not update when attributes did not change' do
+    piotr_rel = double('piotr_rel').as_null_object
+
+    expect(relation).to receive(:by_id).with(piotr[:id]).and_return(piotr_rel)
+    expect(piotr_rel).not_to receive(:update)
+
+    result = users.try do |c|
+      c.update(:by_id, piotr[:id]).change(piotr).to(name: piotr[:name])
+    end
+
+    expect(result.value.to_a).to be_empty
   end
 end
