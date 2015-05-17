15240ffe675adb4621be33e20b49f9b527e452c7
Piotr Solnica <piotr.solnica@gmail.com>
2015-05-17 14:37:35 +0200

    Add some basic docs for transaction

--- lib/rom/sql/commands/transaction.rb
+++ lib/rom/sql/commands/transaction.rb
@@ -3,9 +3,19 @@ require 'rom/commands/result'
 module ROM
   module SQL
     module Commands
+      # Adds transaction interface to commands
+      #
+      # @api private
       module Transaction
         ROM::SQL::Rollback = Class.new(Sequel::Rollback)
 
+        # Start a transaction
+        #
+        # @param [Hash] options The options hash supported by Sequel
+        #
+        # @return [ROM::Commands::Result::Success,ROM::Commands::Result::Failure]
+        #
+        # @api public
         def transaction(options = {}, &block)
           result = relation.dataset.db.transaction(options, &block)
 
