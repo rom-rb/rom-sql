fb0d6a213ef4c0d36d4f57587ea9b329af326bf4
Piotr Solnica <piotr.solnica@gmail.com>
2015-05-20 13:14:05 +0200

    Add yard docs for create command

--- lib/rom/sql/commands/create.rb
+++ lib/rom/sql/commands/create.rb
@@ -5,10 +5,34 @@ require 'rom/sql/commands/transaction'
 module ROM
   module SQL
     module Commands
+      # SQL create command
+      #
+      # @api public
       class Create < ROM::Commands::Create
         include Transaction
         include ErrorWrapper
 
+        # Set command to associate tuples with a parent tuple using provided keys
+        #
+        # @example
+        #   class CreateTask < ROM::Commands::Create[:sql]
+        #     relation :tasks
+        #     associates :user, [:user_id, :id]
+        #   end
+        #
+        #   create_user = rom.command(:user).create.with(name: 'Jane')
+        #
+        #   create_tasks = rom.command(:tasks).create
+        #     .with [{ title: 'One' }, { title: 'Two' } ]
+        #
+        #   command = create_user >> create_tasks
+        #   command.call
+        #
+        # @param [Symbol] name The name of associated table
+        # @param [Hash] options The options
+        # @option options [Array] :key The association keys
+        #
+        # @api public
         def self.associates(name, options)
           option :association, reader: true, default: -> command { options }
 
@@ -23,6 +47,9 @@ module ROM
           end
         end
 
+        # Inserts provided tuples into the database table
+        #
+        # @api private
         def execute(tuples)
           insert_tuples = with_input_tuples(tuples) do |tuple|
             attributes = input[tuple]
@@ -33,11 +60,16 @@ module ROM
           insert(insert_tuples)
         end
 
+        # Executes insert statement and returns inserted tuples
+        #
+        # @api private
         def insert(tuples)
           pks = tuples.map { |tuple| relation.insert(tuple) }
           relation.where(relation.primary_key => pks)
         end
 
+        # Yields tuples for insertion or return an enumerator
+        #
         # @api private
         def with_input_tuples(tuples)
           input_tuples = Array([tuples]).flatten.map
