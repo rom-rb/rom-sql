1fe2ea1556d9c392d0b450548c4b75274a28f77d
James Moriarty <james.moriarty@rea-group.com>
2015-07-27 11:58:41 +1000

    extract files to require.

---
+++ lib/rom/sql/error.rb
@@ -0,0 +1,13 @@
+module ROM
+  module SQL
+    class Error < StandardError
+      attr_reader :original_exception
+
+      def initialize(original_exception)
+        super(original_exception.message)
+        @original_exception = original_exception
+        set_backtrace(original_exception.backtrace)
+      end
+    end
+  end
+end
