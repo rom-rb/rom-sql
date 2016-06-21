98e20842939b482c2103536703aeaa64383078e9
Nikita Shilnikov <ns@latera.ru>
2016-06-22 00:53:45 +0300

    Relax Sequel dependency with checking version at runtime
    * Tested with Sequel versions 4.18.0, 4.33.0, and 4.35.0

--- lib/rom/sql/support/active_support_notifications.rb
+++ lib/rom/sql/support/active_support_notifications.rb
@@ -4,13 +4,24 @@ require 'active_support/notifications'
 module ROM
   module SQL
     module ActiveSupportInstrumentation
-      def log_connection_yield(sql, _conn, args = nil)
-        ActiveSupport::Notifications.instrument(
-          'sql.rom',
-          sql: sql,
-          name: instrumentation_name,
-          binds: args
-        ) { super }
+      if Sequel::MAJOR == 4 && Sequel::MINOR < 35
+        def log_yield(sql, args = nil)
+          ActiveSupport::Notifications.instrument(
+            'sql.rom',
+            sql: sql,
+            name: instrumentation_name,
+            binds: args
+          ) { super }
+        end
+      else
+        def log_connection_yield(sql, _conn, args = nil)
+          ActiveSupport::Notifications.instrument(
+            'sql.rom',
+            sql: sql,
+            name: instrumentation_name,
+            binds: args
+          ) { super }
+        end
       end
 
       private
