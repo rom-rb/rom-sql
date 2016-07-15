e497cb162de01221bf1008b0b6262e0fa69aa534
Piotr Solnica <piotr.solnica@gmail.com>
2016-07-15 14:14:43 +0200

    Add AssociationSet#try
    This is a handy way of handling cases where an association is not
    defined

--- lib/rom/sql/association_set.rb
+++ lib/rom/sql/association_set.rb
@@ -3,6 +3,14 @@ require 'rom/support/registry'
 module ROM
   module SQL
     class AssociationSet < ROM::Registry
+      # @api private
+      def try(name, &block)
+        if key?(name)
+          yield(self[name])
+        else
+          false
+        end
+      end
     end
   end
 end
