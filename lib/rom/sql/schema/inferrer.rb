3c0dec71da99ca674859a419f155fe5c86be6aa0
Nikita Shilnikov <fg@flashgordon.ru>
2016-07-24 15:30:11 +0300

    Infer foreign keys
    This does not support multicolumn foreign key but I bet this is a really rare beast.

--- lib/rom/sql/schema/inferrer.rb
+++ lib/rom/sql/schema/inferrer.rb
@@ -24,11 +24,13 @@ module ROM
           @dsl = dsl
         end
 
+        # @api private
         def call(dataset, gateway)
           columns = gateway.connection.schema(dataset)
+          fks = fks_for(gateway, dataset)
 
           columns.each do |(name, definition)|
-            dsl.attribute name, build_type(definition)
+            dsl.attribute name, build_type(definition.merge(foreign_key: fks[name]))
           end
 
           pks = columns
@@ -40,13 +42,36 @@ module ROM
           dsl.attributes
         end
 
-        def build_type(definition)
-          if definition.fetch(:primary_key)
+        private
+
+        # @api private
+        def build_type(primary_key: , type: , allow_null: , foreign_key: , **rest)
+          if primary_key
             self.class.pk_type
           else
-            type = self.class.type_mapping.fetch(definition.fetch(:type))
-            type = type.optional if definition.fetch(:allow_null)
+            type = self.class.type_mapping.fetch(type)
+            type = type.optional if allow_null
+            type = type.meta(foreign_key: true, relation: foreign_key) if foreign_key
             type
+          end
+        end
+
+        # @api private
+        def fks_for(gateway, dataset)
+          gateway.connection.foreign_key_list(dataset).each_with_object({}) do |definition, fks|
+            column, fk = build_fk(definition)
+
+            fks[column] = fk if fk
+          end
+        end
+
+        # @api private
+        def build_fk(columns: , table: , **rest)
+          if columns.size == 1
+            [columns[0], table]
+          else
+            # We don't have support for multicolumn foreign keys
+            columns[0]
           end
         end
       end
