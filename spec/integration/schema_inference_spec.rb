e5cb3734763608361417b9d72c022089b9495b7e
Nikita Shilnikov <fg@flashgordon.ru>
2016-10-20 02:12:42 +0300

    Add specs for simple NULL/NOT NULL cases

--- spec/integration/schema_inference_spec.rb
+++ spec/integration/schema_inference_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe 'Schema inference for common datatypes' do
   let(:schema) { container.relations[dataset].schema }
 
   with_adapters do
-    context 'inferring attributes' do
-      before do
-        dataset = self.dataset
-        conf.relation(dataset) do
-          schema(dataset, infer: true)
-        end
+    before do
+      dataset = self.dataset
+      conf.relation(dataset) do
+        schema(dataset, infer: true)
       end
+    end
 
+    describe 'inferring attributes' do
       context 'for simple table' do
         let(:dataset) { :users }
 
@@ -31,8 +31,8 @@ RSpec.describe 'Schema inference for common datatypes' do
             id: ROM::SQL::Types::Serial.meta(name: :id),
             title: ROM::SQL::Types::String.optional.meta(name: :title),
             user_id: ROM::SQL::Types::Int.optional.meta(name: :user_id,
-                                                                foreign_key: true,
-                                                                relation: :users)
+                                                        foreign_key: true,
+                                                        relation: :users)
          )
         end
       end
@@ -68,6 +68,62 @@ RSpec.describe 'Schema inference for common datatypes' do
             datetime: ROM::SQL::Types::Time.meta(name: :datetime),
             data: ROM::SQL::Types::Blob.optional.meta(name: :data)
           )
+        end
+      end
+    end
+
+    describe 'using commands with inferred schema' do
+      describe 'inserting' do
+        let(:dataset) { :users }
+        let(:relation) { container.relation(:users) }
+        let(:create) { commands[dataset].create }
+
+        before do
+          conf.commands(:users) do
+            define(:create) do
+              result :one
+            end
+          end
+        end
+
+        it "doesn't coerce or check types on insert by default" do
+          result = create.call(name: Sequel.function(:upper, 'Jade'))
+
+          expect(result).to eql(id: 1, name: 'JADE')
+        end
+
+        context 'nullable columns' do
+          before do
+            conn.add_column :users, :age, :integer, null: true
+          end
+
+          it 'allows to insert records with nil value' do
+            result = create.call(name: 'Jade', age: nil)
+
+            expect(result).to eql(id: 1, name: 'Jade', age: nil)
+          end
+
+          it 'allows to omit nullable columns' do
+            result = create.call(name: 'Jade')
+
+            expect(result).to eql(id: 1, name: 'Jade', age: nil)
+          end
+        end
+
+        context 'columns with default value' do
+          before do
+            conn.add_column :users, :age, :integer, null: false, default: 18
+          end
+
+          it 'sets default value on missing key' do
+            result = create.call(name: 'Jade')
+
+            expect(result).to eql(id: 1, name: 'Jade', age: 18)
+          end
+
+          it 'raises an error on inserting nil value' do
+            expect { create.call(name: 'Jade', age: nil) }.to raise_error(ROM::SQL::NotNullConstraintError)
+          end
         end
       end
     end
