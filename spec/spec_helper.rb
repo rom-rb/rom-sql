4db60edd82c868e6ae2dc84d2fc2e1aad2777a8b
Nikita Shilnikov <fg@flashgordon.ru>
2016-11-07 13:51:45 +0300

    Exclude spec dir from coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,9 @@ Bundler.setup
 
 if RUBY_ENGINE == 'ruby' && RUBY_VERSION == '2.3.1'
   require 'simplecov'
-  SimpleCov.start
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
 end
 
 require 'rom-sql'
