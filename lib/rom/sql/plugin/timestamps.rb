029f263fac27f9cdeca7ecf87066eb8c68edff5c
Chris Flipse <cflipse@gmail.com>
2016-11-19 18:53:49 -0500

    rubocop sweep

--- lib/rom/sql/plugin/timestamps.rb
+++ lib/rom/sql/plugin/timestamps.rb
@@ -4,7 +4,6 @@ module ROM
   module SQL
     module Plugin
       module Timestamps
-
         def self.included(klass)
           klass.extend(ClassInterface)
           super
@@ -17,40 +16,46 @@ module ROM
             super
           end
 
-
           def timestamps(*args)
             timestamp_columns timestamp_columns.merge(args)
 
             include InstanceMethods
           end
-          alias_method :timestamp, :timestamps
+          alias timestamp timestamps
         end
 
         module InstanceMethods
-
           # @api private
           def timestamp_columns
             self.class.timestamp_columns
           end
 
           def execute(tuples)
-            time        = Time.now.utc
-            timestamps  = {}
-            timestamp_columns.each do |column|
-              timestamps[column.to_sym]  = time
-            end
+            timestamps = build_timestamps
 
             input_tuples = case tuples
                            when Hash
                              timestamps.merge(tuples)
                            when Array
-                             tuples.map{|t| timestamps.merge(t) }
+                             tuples.map { |t| timestamps.merge(t) }
                            end
 
             super input_tuples
           end
-        end
 
+          private
+
+          # @api private
+          def build_timestamps
+            time        = Time.now.utc
+            timestamps  = {}
+            timestamp_columns.each do |column|
+              timestamps[column.to_sym] = time
+            end
+
+            timestamps
+          end
+        end
       end
     end
   end
