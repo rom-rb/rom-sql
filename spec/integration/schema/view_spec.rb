f284ab3ff6cfceb514063e228576640204e09f52
Piotr Solnica <piotr.solnica+oss@gmail.com>
2016-12-08 21:23:52 +0100

    Add Schema#project_relation

---
+++ spec/integration/schema/view_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+RSpec.describe 'Defining a view using schemas' do
+  include_context 'database setup'
+
+  with_adapters do
+    describe 'defining a projected view' do
+      before do
+        conf.relation(:users) do
+          schema(infer: true)
+
+          view(:names) do
+            schema { project(:name) }
+            relation { order(:name, :id) }
+          end
+        end
+
+        container.relations[:users].insert(name: 'Joe')
+        container.relations[:users].insert(name: 'Jane')
+        container.relations[:users].insert(name: 'Jade')
+      end
+
+      it 'automatically projects a relation view' do
+        expect(relations[:users].names.to_a)
+          .to eql([{ name: 'Jade' }, { name: 'Jane' }, { name: 'Joe' }])
+      end
+    end
+  end
+end
