7335128f7b2e29aaae160d83f09f7dfd762ee1b3
Piotr Solnica <piotr.solnica+oss@gmail.com>
2016-12-09 13:42:59 +0100

    Define default views when we already have a schema

--- lib/rom/sql/relation.rb
+++ lib/rom/sql/relation.rb
@@ -53,6 +53,9 @@ module ROM
           end
 
           dataset do
+            # TODO: feels strange to do it here - we need a new hook for this during finalization
+            klass.define_default_views!
+
             table = opts[:from].first
 
             if db.table_exists?(table)
@@ -63,15 +66,18 @@ module ROM
               self
             end
           end
+        end
+      end
 
-          # @!method by_pk(pk)
-          #   Return a relation restricted by its primary key
-          #   @param [Object] pk The primary key value
-          #   @return [SQL::Relation]
-          #   @api public
-          view(:by_pk, attributes[:base]) do |pk|
-            where(primary_key => pk)
-          end
+      # @api private
+      def self.define_default_views!
+        # @!method by_pk(pk)
+        #   Return a relation restricted by its primary key
+        #   @param [Object] pk The primary key value
+        #   @return [SQL::Relation]
+        #   @api public
+        view(:by_pk, attributes[:base]) do |pk|
+          where(primary_key => pk)
         end
       end
 
