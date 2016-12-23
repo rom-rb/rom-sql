223549aacb2bd7cb2611921df5f7e6730d4933c4
Piotr Solnica <piotr.solnica+oss@gmail.com>
2016-12-23 13:36:38 +0100

    Add a spec for Schema#prefix projection

---
+++ spec/integration/schema/prefix_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+RSpec.describe ROM::SQL::Schema, '#prefix' do
+  include_context 'database setup'
+
+  with_adapters :postgres do
+    before do
+      conf.relation(:users) do
+        schema(infer: true)
+      end
+    end
+
+    it 'auto-projects a relation with renamed columns using provided prefix' do
+      expect(relations[:users].schema.prefix(:user).(relations[:users]).dataset.sql)
+        .to eql('SELECT "id" AS "user_id", "name" AS "user_name" FROM "users" ORDER BY "users"."id"')
+    end
+  end
+end
