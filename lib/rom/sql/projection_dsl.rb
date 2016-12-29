d5a91af155fdc65912f2bf47a7ae936b9a60055f
Piotr Solnica <piotr.solnica+oss@gmail.com>
2016-12-29 10:25:11 +0100

    Extract SQL::Function to a separate file

--- lib/rom/sql/projection_dsl.rb
+++ lib/rom/sql/projection_dsl.rb
@@ -1,31 +1,7 @@
+require 'rom/sql/function'
+
 module ROM
   module SQL
-    class Function < ROM::Schema::Type
-      def as(name)
-        meta(name: name)
-      end
-
-      def sql_literal_append(ds, sql)
-        ds.literal_append(sql, func.as(name))
-      end
-
-      if RUBY_VERSION < '2.3'
-        def to_ary
-          [self]
-        end
-      end
-
-      private
-
-      def func
-        Sequel::SQL::Function.new(meta[:op], *meta[:args])
-      end
-
-      def method_missing(op, *args)
-        meta(op: op, args: args)
-      end
-    end
-
     class ProjectionDSL < BasicObject
       attr_reader :schema
 
