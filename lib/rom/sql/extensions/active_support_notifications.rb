dad07eaba528003c4bb7d57c1a10fce680e81c52
Nikita Shilnikov <fg@flashgordon.ru>
2017-01-18 20:40:20 +0700

    Remove version check for Sequel
    We're now at 4.42 minimum

--- lib/rom/sql/extensions/active_support_notifications.rb
+++ lib/rom/sql/extensions/active_support_notifications.rb
@@ -4,24 +4,13 @@ require 'active_support/notifications'
 module ROM
   module SQL
     module ActiveSupportInstrumentation
-      if Sequel::MAJOR == 4 && Sequel::MINOR < 35
-        def log_yield(sql, args = nil)
-          ActiveSupport::Notifications.instrument(
-            'sql.rom',
-            sql: sql,
-            name: instrumentation_name,
-            binds: args
-          ) { super }
-        end
-      else
-        def log_connection_yield(sql, _conn, args = nil)
-          ActiveSupport::Notifications.instrument(
-            'sql.rom',
-            sql: sql,
-            name: instrumentation_name,
-            binds: args
-          ) { super }
-        end
+      def log_connection_yield(sql, _conn, args = nil)
+        ActiveSupport::Notifications.instrument(
+          'sql.rom',
+          sql: sql,
+          name: instrumentation_name,
+          binds: args
+        ) { super }
       end
 
       private
