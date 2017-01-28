15485c1f32262801bfcfd79b1a17386a72cef9dd
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-01-29 00:22:57 +0100

    Add a spec covering order by a virtual attribute

--- spec/unit/relation/order_spec.rb
+++ spec/unit/relation/order_spec.rb
@@ -37,4 +37,15 @@ RSpec.describe ROM::Relation, '#order' do
         to eql([{ id: 2, name: 'Joe' }, { id: 1, name: 'Jane' }, { id: 3, name: 'Jade' }])
     end
   end
+
+  with_adapters :postgres, :mysql do
+    it 'orders by virtual attributes' do
+      ordered = relation.
+                  select { string::concat(id, '-', name).as(:uid) }.
+                  order(:uid)
+
+      expect(ordered.to_a).
+        to eql([{ uid: '1-Jane' }, { uid: '2-Joe' }, { uid: '3-Jade' }])
+    end
+  end
 end
