7a5de3c8866e522540b0253bb8a95e2f4ec3a11c
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-03-01 20:06:19 +0100

    Add a spec for auto_restrictions with explicit schema

--- spec/integration/plugins/auto_restrictions_spec.rb
+++ spec/integration/plugins/auto_restrictions_spec.rb
@@ -4,15 +4,40 @@ RSpec.describe 'Plugins / :auto_restrictions', seeds: true do
   with_adapters do
     before do
       conn.add_index :tasks, :title, unique: true
-      conf.plugin(:sql, relations: :auto_restrictions)
     end
 
-    it 'defines restriction views for all indexed attributes' do
-      expect(tasks.select(:id).by_title("Jane's task").one).to eql(id: 2)
+    shared_context 'auto-generated restriction view' do
+      it 'defines restriction views for all indexed attributes' do
+        expect(tasks.select(:id).by_title("Jane's task").one).to eql(id: 2)
+      end
+
+      it 'defines curried methods' do
+        expect(tasks.by_title.("Jane's task").first).to eql(id: 2, user_id: 1, title: "Jane's task")
+      end
     end
 
-    it 'defines curried methods' do
-      expect(tasks.by_title.("Jane's task").first).to eql(id: 2, user_id: 1, title: "Jane's task")
+    context 'with an inferred schema' do
+      before do
+        conf.plugin(:sql, relations: :auto_restrictions)
+      end
+
+      include_context 'auto-generated restriction view'
+    end
+
+    context 'with explicit schema' do
+      before do
+        conf.relation(:tasks) do
+          schema do
+            attribute :id, ROM::SQL::Types::Serial
+            attribute :user_id, ROM::SQL::Types::Int
+            attribute :title, ROM::SQL::Types::String.meta(index: true)
+          end
+
+          use :auto_restrictions
+        end
+      end
+
+      include_context 'auto-generated restriction view'
     end
   end
 end
