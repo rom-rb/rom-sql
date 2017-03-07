07095c3e4aa4e195ba826e18b3f34e32f77b2280
Nikita Shilnikov <fg@flashgordon.ru>
2017-03-07 11:38:00 +0300

    Add example not handled by join by relation

--- spec/unit/relation/inner_join_spec.rb
+++ spec/unit/relation/inner_join_spec.rb
@@ -3,6 +3,7 @@ RSpec.describe ROM::Relation, '#inner_join' do
 
   let(:tasks) { relations[:tasks] }
   let(:tags) { relations[:tags] }
+  let(:puzzles) { relations[:puzzles] }
 
   include_context 'users and tasks'
 
@@ -39,6 +40,16 @@ RSpec.describe ROM::Relation, '#inner_join' do
 
     context 'with associations' do
       before do
+        inferrable_relations.concat %i(puzzles)
+      end
+
+      before do
+        conn.create_table(:puzzles) do
+          primary_key :id
+          foreign_key :author_id, :users, null: false
+          column :text, String, null: false
+        end
+
         conf.relation(:users) do
           schema(infer: true) do
             associations do
@@ -67,7 +78,16 @@ RSpec.describe ROM::Relation, '#inner_join' do
           end
         end
 
+        conf.relation(:puzzles) do
+          schema(infer: true) do
+            associations do
+              belongs_to :users, as: :author
+            end
+          end
+        end
+
         relation.insert id: 3, name: 'Jade'
+        puzzles.insert id: 1, author_id: 1, text: 'solved by Jane'
       end
 
       it 'joins relation with join keys inferred' do
@@ -95,6 +115,11 @@ RSpec.describe ROM::Relation, '#inner_join' do
                    select(:name, tasks[:title])
 
         expect(result.schema.map(&:name)).to eql(%i[name title])
+
+        expect(result.to_a).to eql([
+                                     { name: 'Jane', title: "Jane's task" },
+                                     { name: 'Joe', title: "Joe's task" }
+                                   ])
       end
 
       it 'joins if association name differs from relation name' do
@@ -103,6 +128,17 @@ RSpec.describe ROM::Relation, '#inner_join' do
                    select(:name, tasks[:title])
 
         expect(result.schema.map(&:name)).to eql(%i[name title])
+
+        expect(result.to_a).to eql([
+                                     { name: 'Jane', title: "Jane's task" },
+                                     { name: 'Joe', title: "Joe's task" }
+                                   ])
+      end
+
+      it 'joins by relation if association name differs from relation name' do
+        result = puzzles.inner_join(users).select(:name, puzzles[:text])
+
+        expect(result.to_a).to eql([ name: 'Jane', title: "Jane's task" ])
       end
     end
 
