bd2dcd10f5b71d368a74d99a84e3b42749567570
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-05-01 12:45:04 +0200

    Replace integration spec for m-to-m + associates with a unit spec
    This clearly shows the level of coupling in associates + associations.
    We definitely need better encapsulation. I'm holding off with a bigger
    refactoring because it needs to wait for 4.0 where we'll introduce
    run-time associations. I'll only try to clean this up a little bit for
    now.

---
+++ spec/unit/plugin/associates_spec.rb
@@ -0,0 +1,108 @@
+require 'ostruct'
+require 'rom/sql/commands'
+
+RSpec.describe ROM::SQL::Plugin::Associates do
+  subject(:command) do
+    command_class.build(posts).with_association(:tags)
+  end
+
+  let(:posts) do
+    instance_double(Class.new(ROM::SQL::Relation), schema?: false, associations: associations)
+  end
+
+  let(:tags) do
+    instance_double(ROM::SQL::Relation, associations: associations)
+  end
+
+  let(:join_relation) do
+    instance_double(ROM::SQL::Relation)
+  end
+
+  let(:registry) do
+    Hash.new { |h, k| h.fetch(k.to_sym) }.update(posts: posts, tags: tags)
+  end
+
+  let(:command_class) do
+    Class.new(ROM::SQL::Commands::Create) do
+      use :associates, tags: []
+    end
+  end
+
+  let(:associations) do
+    Hash.new { |h, k| h.fetch(k.to_sym) }.update(posts: posts_assoc)
+  end
+
+  let(:tags_assoc) do
+    ROM::SQL::Association::ManyToMany.new(:posts, :tags, through: :posts_tags)
+  end
+
+  let(:posts_assoc) do
+    ROM::SQL::Association::ManyToMany.new(:tags, :posts, through: :posts_tags)
+  end
+
+  before do
+    allow(posts).to receive(:__registry__).and_return(registry)
+    allow(associations).to receive(:try).and_yield(tags_assoc)
+    allow(tags_assoc).to receive(:join_keys).and_return({})
+  end
+
+  shared_context 'associates result' do
+    it 'inserts join tuples and returns child tuples with combine keys' do
+      join_tuples = [
+        { post_id: 1, tag_id: 1 }, { post_id: 1, tag_id: 2 },
+        { post_id: 2, tag_id: 1 }, { post_id: 2, tag_id: 2 }
+      ]
+
+      expect(tags_assoc).
+        to receive(:associate).with(registry, post_tuples, tag_tuples).and_return(join_tuples)
+
+      expect(tags_assoc).to receive(:join_relation).and_return(join_relation)
+      expect(join_relation).to receive(:multi_insert).with(join_tuples)
+      expect(posts_assoc).to receive(:combine_keys).and_return(name: :tag)
+
+      result = command.associate(post_tuples, tag_tuples, assoc: tags_assoc, keys: {})
+
+      expect(result).
+        to match_array([
+                         { title: 'post 1', tag: 'red' }, { title: 'post 1', tag: 'green'},
+                         { title: 'post 2', tag: 'red' }, { title: 'post 2', tag: 'green'}
+                       ])
+    end
+  end
+
+  describe '#associate' do
+    context 'with plain hash tuples' do
+      let(:post_tuples) do
+        [{ title: 'post 1' }, { title: 'post 2' }]
+      end
+
+      let(:tag_tuples) do
+        [{ name: 'red' }, { name: 'green' }]
+      end
+
+      include_context 'associates result'
+    end
+
+    context 'with tuples coercible to a hash' do
+      before do
+        module Test
+          class Post < OpenStruct
+            def to_hash
+              { title: title }
+            end
+          end
+        end
+      end
+
+      let(:post_tuples) do
+        [Test::Post.new(title: 'post 1'), Test::Post.new(title: 'post 2')]
+      end
+
+      let(:tag_tuples) do
+        [{ name: 'red' }, { name: 'green' }]
+      end
+
+      include_context 'associates result'
+    end
+  end
+end
