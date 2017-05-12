641c9e19782dc630b33afe758145fe65c0b628f7
Nikita Shilnikov <fg@flashgordon.ru>
2017-05-12 13:55:49 +0300

    YARD for PG::Array

--- lib/rom/sql/extensions/postgres/types.rb
+++ lib/rom/sql/extensions/postgres/types.rb
@@ -20,9 +20,98 @@ module ROM
           Array.constructor(-> (v) { Sequel.pg_array(v, db_type) }).meta(type: db_type)
         end
 
+        # @!parse
+        #   class ROM::SQL::Attribute
+        #     # @!method contain(other)
+        #     #   Check whether the array includes another array
+        #     #   Translates to the @> operator
+        #     #
+        #     #   @param [Array] other
+        #     #
+        #     #   @return [SQL::Attribute<Types::Bool>]
+        #     #
+        #     #   @api public
+        #
+        #     # @!method get(idx)
+        #     #   Get element by index (PG uses 1-based indexing)
+        #     #
+        #     #   @param [Integer] idx
+        #     #
+        #     #   @return [SQL::Attribute]
+        #     #
+        #     #   @api public
+        #
+        #     # @!method any(value)
+        #     #   Check whether the array includes a value
+        #     #   Translates to the ANY operator
+        #     #
+        #     #   @param [Object] value
+        #     #
+        #     #   @return [SQL::Attribute<Types::Bool>]
+        #     #
+        #     #   @api public
+        #
+        #     # @!method contained_by(other)
+        #     #   Check whether the array is contained by another array
+        #     #   Translates to the <@ operator
+        #     #
+        #     #   @param [Array] other
+        #     #
+        #     #   @return [SQL::Attribute<Types::Bool>]
+        #     #
+        #     #   @api public
+        #
+        #     # @!method length
+        #     #   Return array size
+        #     #
+        #     #   @return [SQL::Attribute<Types::Int>]
+        #     #
+        #     #   @api public
+        #
+        #     # @!method overlaps(other)
+        #     #   Check whether the arrays have common values
+        #     #   Translates to &&
+        #     #
+        #     #   @param [Array] other
+        #     #
+        #     #   @return [SQL::Attribute<Types::Bool>]
+        #     #
+        #     #   @api public
+        #
+        #     # @!method remove_value(value)
+        #     #   Remove elements by value
+        #     #
+        #     #   @param [Object] value
+        #     #
+        #     #   @return [SQL::Attribute<Types::PG::Array>]
+        #     #
+        #     #   @api public
+        #
+        #     # @!method join(delimiter, null_repr)
+        #     #   Convert the array to a string by joining
+        #     #   values with a delimiter (empty stirng by default)
+        #     #   and optional filler for NULL values
+        #     #   Translates to an `array_to_string` call
+        #     #
+        #     #   @param [Object] delimiter
+        #     #   @param [Object] null
+        #     #
+        #     #   @return [SQL::Attribute<Types::String>]
+        #     #
+        #     #   @api public
+        #
+        #     # @!method +(other)
+        #     #   Concatenate two arrays
+        #     #
+        #     #   @param [Array] other
+        #     #
+        #     #   @return [SQL::Attribute<Types::PG::Array>]
+        #     #
+        #     #   @api public
+        #   end
         Attribute::TypeExtensions.register(Array.constructor -> {  }) do
-          def contain(type, expr, value)
-            Attribute[Types::Bool].meta(sql_expr: expr.pg_array.contains(type[value]))
+          def contain(type, expr, other)
+            Attribute[Types::Bool].meta(sql_expr: expr.pg_array.contains(type[other]))
           end
 
           def get(type, expr, idx)
@@ -33,8 +122,8 @@ module ROM
             Attribute[Types::Bool].meta(sql_expr: { value => expr.pg_array.any })
           end
 
-          def contained_by(type, expr, value)
-            Attribute[Types::Bool].meta(sql_expr: expr.pg_array.contained_by(type[value]))
+          def contained_by(type, expr, other)
+            Attribute[Types::Bool].meta(sql_expr: expr.pg_array.contained_by(type[other]))
           end
 
           def length(type, expr)
