1ff70e15cadc2daa47727f21e3ff56758fbe7857
Nikita Shilnikov <fg@flashgordon.ru>
2017-06-21 02:04:41 +0300

    Improve spec code

--- spec/unit/schema_spec.rb
+++ spec/unit/schema_spec.rb
@@ -1,20 +1,22 @@
 RSpec.describe ROM::SQL::Schema, :postgres do
   describe '#primary_key' do
     it 'returns primary key attributes' do
-      schema = Class.new(ROM::Relation[:sql]).schema do
+      schema_proc = Class.new(ROM::Relation[:sql]).schema do
         attribute :id, ROM::SQL::Types::Serial
-      end.call
+      end
 
+      schema = schema_proc.call
       schema.finalize_attributes!.finalize!
 
       expect(schema.primary_key).to eql([schema[:id]])
     end
 
     it 'returns empty array when there is no PK defined' do
-      schema = Class.new(ROM::Relation[:sql]).schema do
+      schema_proc = Class.new(ROM::Relation[:sql]).schema do
         attribute :id, ROM::SQL::Types::Int
-      end.call
+      end
 
+      schema = schema_proc.call
       schema.finalize_attributes!.finalize!
 
       expect(schema.primary_key).to eql([])
