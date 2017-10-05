de27b4cd262e7c5a8e37d34b26b016f4980d2dd6
GustavoCaso <gustavocaso@gmail.com>
2017-10-05 08:25:43 +0200

    [PG] added meta data for geometric types

--- lib/rom/sql/extensions/postgres/types/geometric.rb
+++ lib/rom/sql/extensions/postgres/types/geometric.rb
@@ -32,100 +32,114 @@ module ROM
         # The list of geometric data types supported by PostgreSQL
         # @see https://www.postgresql.org/docs/current/static/datatype-geometric.html
 
-        Point = SQL::Types.define(Values::Point) do
-          input do |point|
-            "(#{ point.x },#{ point.y })"
-          end
+        Point = Type('point') do
+            SQL::Types.define(Values::Point) do
+            input do |point|
+              "(#{ point.x },#{ point.y })"
+            end
 
-          output do |point|
-            x, y = point.to_s[1...-1].split(',', 2)
-            Values::Point.new(Float(x), Float(y))
-          end
-        end
-
-        Line = SQL::Types.define(Values::Line) do
-          input do |line|
-            "{#{ line.a },#{ line.b },#{line.c}}"
-          end
-
-          output do |line|
-            a, b, c = line.to_s[1..-2].split(',', 3)
-            Values::Line.new(Float(a), Float(b), Float(c))
-          end
-        end
-
-        Circle = SQL::Types.define(Values::Circle) do
-          input do |circle|
-            "<(#{ circle.center.x },#{ circle.center.y }),#{ circle.radius }>"
-          end
-
-          output do |circle|
-            x, y, r = circle.to_s.tr('()<>', '').split(',', 3)
-            center = Values::Point.new(Float(x), Float(y))
-            Values::Circle.new(center, Float(r))
-          end
-        end
-
-        Box = SQL::Types.define(Values::Box) do
-          input do |box|
-            "((#{ box.upper_right.x },#{ box.upper_right.y }),"\
-            "(#{ box.lower_left.x },#{ box.lower_left.y }))"
-          end
-
-          output do |box|
-            x_right, y_right, x_left, y_left = box.to_s.tr('()', '').split(',', 4)
-            upper_right = Values::Point.new(Float(x_right), Float(y_right))
-            lower_left = Values::Point.new(Float(x_left), Float(y_left))
-            Values::Box.new(upper_right, lower_left)
-          end
-        end
-
-        LineSegment = SQL::Types.define(Values::LineSegment) do
-          input do |segment|
-            "[(#{ segment.begin.x },#{ segment.begin.y }),"\
-            "(#{ segment.end.x },#{ segment.end.y })]"
-          end
-
-          output do |segment|
-            x_begin, y_begin, x_end, y_end = segment.to_s.tr('()[]', '').split(',', 4)
-            point_begin = Values::Point.new(Float(x_begin), Float(y_begin))
-            point_end = Values::Point.new(Float(x_end), Float(y_end))
-            Values::LineSegment.new(point_begin, point_end)
-          end
-        end
-
-        Polygon = SQL::Types.define(::Array) do
-          input do |points|
-            points_joined = points.map { |p| "(#{ p.x },#{ p.y })" }.join(',')
-            "(#{ points_joined })"
-          end
-
-          output do |polygon|
-            coordinates = polygon.to_s.tr('()', '').split(',').each_slice(2)
-            coordinates.map { |x, y| Values::Point.new(Float(x), Float(y)) }
-          end
-        end
-
-        Path = SQL::Types.define(Values::Path) do
-          input do |path|
-            points_joined = path.to_a.map { |p| "(#{ p.x },#{ p.y })" }.join(',')
-
-            if path.open?
-              "[#{ points_joined }]"
-            else
-              "(#{ points_joined })"
+            output do |point|
+              x, y = point.to_s[1...-1].split(',', 2)
+              Values::Point.new(Float(x), Float(y))
             end
           end
+        end
 
-          output do |path|
-            open = path.to_s.start_with?('[') && path.to_s.end_with?(']')
-            coordinates = path.to_s.tr('()[]', '').split(',').each_slice(2)
-            points = coordinates.map { |x, y| Values::Point.new(Float(x), Float(y)) }
+        Line = Type('line') do
+          SQL::Types.define(Values::Line) do
+            input do |line|
+              "{#{ line.a },#{ line.b },#{line.c}}"
+            end
 
-            if open
-              Values::Path.new(points, :open)
-            else
-              Values::Path.new(points, :closed)
+            output do |line|
+              a, b, c = line.to_s[1..-2].split(',', 3)
+              Values::Line.new(Float(a), Float(b), Float(c))
+            end
+          end
+        end
+
+        Circle = Type('circle') do
+          SQL::Types.define(Values::Circle) do
+            input do |circle|
+              "<(#{ circle.center.x },#{ circle.center.y }),#{ circle.radius }>"
+            end
+
+            output do |circle|
+              x, y, r = circle.to_s.tr('()<>', '').split(',', 3)
+              center = Values::Point.new(Float(x), Float(y))
+              Values::Circle.new(center, Float(r))
+            end
+          end
+        end
+
+        Box = Type('box') do
+          SQL::Types.define(Values::Box) do
+            input do |box|
+              "((#{ box.upper_right.x },#{ box.upper_right.y }),"\
+              "(#{ box.lower_left.x },#{ box.lower_left.y }))"
+            end
+
+            output do |box|
+              x_right, y_right, x_left, y_left = box.to_s.tr('()', '').split(',', 4)
+              upper_right = Values::Point.new(Float(x_right), Float(y_right))
+              lower_left = Values::Point.new(Float(x_left), Float(y_left))
+              Values::Box.new(upper_right, lower_left)
+            end
+          end
+        end
+
+        LineSegment = Type('lseg') do
+          SQL::Types.define(Values::LineSegment) do
+            input do |segment|
+              "[(#{ segment.begin.x },#{ segment.begin.y }),"\
+              "(#{ segment.end.x },#{ segment.end.y })]"
+            end
+
+            output do |segment|
+              x_begin, y_begin, x_end, y_end = segment.to_s.tr('()[]', '').split(',', 4)
+              point_begin = Values::Point.new(Float(x_begin), Float(y_begin))
+              point_end = Values::Point.new(Float(x_end), Float(y_end))
+              Values::LineSegment.new(point_begin, point_end)
+            end
+          end
+        end
+
+        Polygon = Type('polygon') do
+          SQL::Types.define(::Array) do
+            input do |points|
+              points_joined = points.map { |p| "(#{ p.x },#{ p.y })" }.join(',')
+              "(#{ points_joined })"
+            end
+
+            output do |polygon|
+              coordinates = polygon.to_s.tr('()', '').split(',').each_slice(2)
+              coordinates.map { |x, y| Values::Point.new(Float(x), Float(y)) }
+            end
+          end
+        end
+
+        Path = Type('path') do
+          SQL::Types.define(Values::Path) do
+            input do |path|
+              points_joined = path.to_a.map { |p| "(#{ p.x },#{ p.y })" }.join(',')
+
+              if path.open?
+                "[#{ points_joined }]"
+              else
+                "(#{ points_joined })"
+              end
+            end
+
+            output do |path|
+              open = path.to_s.start_with?('[') && path.to_s.end_with?(']')
+              coordinates = path.to_s.tr('()[]', '').split(',').each_slice(2)
+              points = coordinates.map { |x, y| Values::Point.new(Float(x), Float(y)) }
+
+              if open
+                Values::Path.new(points, :open)
+              else
+                Values::Path.new(points, :closed)
+              end
             end
           end
         end
