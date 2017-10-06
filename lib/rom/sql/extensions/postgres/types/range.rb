275a509c8a47d4374607e0621531be2d6f6fa147
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2017-10-07 02:51:21 +0300

    Add PG range functions and operators

--- lib/rom/sql/extensions/postgres/types/range.rb
+++ lib/rom/sql/extensions/postgres/types/range.rb
@@ -1,6 +1,6 @@
 require 'sequel/core'
 
-Sequel.extension(:pg_range)
+Sequel.extension(:pg_range, :pg_range_ops)
 
 module ROM
   module SQL
@@ -49,6 +49,130 @@ module ROM
         TsRange   = Range('tsrange',   SQL::Types::Form::Time)
         TsTzRange = Range('tstzrange', SQL::Types::Form::Time)
         DateRange = Range('daterange', SQL::Types::Form::Date)
+
+        module RangeOperators
+          def contains(_type, expr, value)
+            Attribute[SQL::Types::Bool].meta(
+              sql_expr: Sequel.pg_range(expr).contains(value)
+            )
+          end
+
+          def contained_by(_type, expr, value)
+            Attribute[SQL::Types::Bool].meta(
+              sql_expr: Sequel.pg_range(expr).contained_by(value)
+            )
+          end
+
+          def overlaps(_type, expr, value)
+            Attribute[SQL::Types::Bool].meta(
+              sql_expr: Sequel.pg_range(expr).overlaps(value)
+            )
+          end
+
+          def left_of(_type, expr, value)
+            Attribute[SQL::Types::Bool].meta(
+              sql_expr: Sequel.pg_range(expr).left_of(value)
+            )
+          end
+
+          def right_of(_type, expr, value)
+            Attribute[SQL::Types::Bool].meta(
+              sql_expr: Sequel.pg_range(expr).right_of(value)
+            )
+          end
+
+          def starts_after(_type, expr, value)
+            Attribute[SQL::Types::Bool].meta(
+              sql_expr: Sequel.pg_range(expr).starts_after(value)
+            )
+          end
+
+          def ends_before(_type, expr, value)
+            Attribute[SQL::Types::Bool].meta(
+              sql_expr: Sequel.pg_range(expr).ends_before(value)
+            )
+          end
+
+          def adjacent_to(_type, expr, value)
+            Attribute[SQL::Types::Bool].meta(
+              sql_expr: Sequel.pg_range(expr).adjacent_to(value)
+            )
+          end
+        end
+
+        module RangeFunctions
+          def lower(_type, expr)
+            Attribute[SQL::Types::Bool].meta(
+              sql_expr: Sequel.pg_range(expr).lower
+            )
+          end
+
+          def upper(_type, expr)
+            Attribute[SQL::Types::Bool].meta(
+              sql_expr: Sequel.pg_range(expr).upper
+            )
+          end
+
+          def isempty(_type, expr)
+            Attribute[SQL::Types::Bool].meta(
+              sql_expr: Sequel.pg_range(expr).isempty
+            )
+          end
+
+          def lower_inc(_type, expr)
+            Attribute[SQL::Types::Bool].meta(
+              sql_expr: Sequel.pg_range(expr).lower_inc
+            )
+          end
+
+          def upper_inc(_type, expr)
+            Attribute[SQL::Types::Bool].meta(
+              sql_expr: Sequel.pg_range(expr).upper_inc
+            )
+          end
+
+          def lower_inf(_type, expr)
+            Attribute[SQL::Types::Bool].meta(
+              sql_expr: Sequel.pg_range(expr).lower_inf
+            )
+          end
+
+          def upper_inf(_type, expr)
+            Attribute[SQL::Types::Bool].meta(
+              sql_expr: Sequel.pg_range(expr).upper_inf
+            )
+          end
+        end
+
+        TypeExtensions.register(Int4Range) do
+          include RangeOperators
+          include RangeFunctions
+        end
+
+        TypeExtensions.register(Int8Range) do
+          include RangeOperators
+          include RangeFunctions
+        end
+
+        TypeExtensions.register(NumRange) do
+          include RangeOperators
+          include RangeFunctions
+        end
+
+        TypeExtensions.register(TsRange) do
+          include RangeOperators
+          include RangeFunctions
+        end
+
+        TypeExtensions.register(TsTzRange) do
+          include RangeOperators
+          include RangeFunctions
+        end
+
+        TypeExtensions.register(DateRange) do
+          include RangeOperators
+          include RangeFunctions
+        end
       end
     end
   end
---
+++ spec/extensions/postgres/attribute/range_spec.rb
@@ -0,0 +1,154 @@
+RSpec.describe 'ROM::SQL::Attribute', :postgres do
+  include_context 'database setup'
+
+  def create_ranges_table(db_type, values)
+    conn.create_table :pg_ranges do
+      primary_key :id
+      text :name
+
+      send(db_type, :range)
+    end
+
+    conf.relation(:pg_ranges) do
+      schema(:pg_ranges, infer: true)
+    end
+
+    conf.commands(:pg_ranges) do
+      define(:create)
+    end
+
+    values.each do |key, value|
+      commands[:pg_ranges].create.(name: key.to_s, range: value)
+    end
+  end
+
+  shared_examples 'range type' do
+    let(:rel) { pg_ranges. select { [name] } }
+
+    it 'restrict by contains (`@>`)' do
+      expect(rel.where(pg_ranges[:range].contains(ref_value)).to_a)
+        .to eql([{ name: 'containing' }])
+    end
+
+    it 'restrict by contained_by (`<@`)' do
+      expect(rel.where(pg_ranges[:range].contained_by(ref_value)).to_a)
+        .to eql([{ name: 'contained' }])
+    end
+
+    it 'restrict by strict left of (`<<`)' do
+      expect(rel.where(pg_ranges[:range].left_of(ref_value)).to_a)
+        .to eql([{ name: 'left' }])
+    end
+
+    it 'restrict by strict right of (`>>`)' do
+      expect(rel.where(pg_ranges[:range].right_of(ref_value)).to_a)
+        .to eql([{ name: 'right' }])
+    end
+
+    it 'restrict by does not extend to the right of (`&<`)' do
+      expect(rel.where(pg_ranges[:range].ends_before(ref_value)).to_a)
+        .to eql([{ name: 'contained' }, { name: 'left' }])
+    end
+
+    it 'restrict by does not extend to the left of (`&>`)' do
+      expect(rel.where(pg_ranges[:range].starts_after(ref_value)).to_a)
+        .to eql([{ name: 'contained' }, { name: 'right' }])
+    end
+
+    it 'restrict by overlapping (`&&`)' do
+      expect(rel.where(pg_ranges[:range].overlaps(ref_value)).to_a)
+        .to eql([{ name: 'containing' }, { name: 'contained' }])
+    end
+
+    it 'restrict by adjacent to (`-|-`)' do
+      expect(rel.where(pg_ranges[:range].adjacent_to(ref_value)).to_a)
+        .to eql([{ name: 'left' }])
+    end
+  end
+
+  describe 'Postgres range types' do
+    let(:pg_ranges) { relations[:pg_ranges] }
+    let(:range_value) { ROM::SQL::Postgres::Values::Range }
+
+    before do
+      conn.extension(:pg_range)
+      conn.drop_table?(:pg_ranges)
+      create_ranges_table(db_type, values)
+    end
+
+    describe 'int4range' do
+      let(:db_type) { :int4range }
+
+      let(:values) do
+        {
+          containing: range_value.new(3, 9, :'[', :']'),
+          contained: range_value.new(5, 7, :'[', :')'),
+          left: range_value.new(1, 3, :'(', :']'),
+          right: range_value.new(9, 12, :'(', :')')
+        }
+      end
+
+      let(:ref_value) { '[4,8]' }
+
+      it_behaves_like 'range type'
+    end
+
+    describe 'tsrange' do
+      let(:db_type) { :tsrange }
+
+      let(:values) do
+        {
+          containing: range_value.new(
+            Time.parse('2017-09-25 03:00:00'),
+            Time.parse('2017-09-25 09:00:00'), :'[', :']'
+          ),
+          contained: range_value.new(
+            Time.parse('2017-09-25 05:00:00'),
+            Time.parse('2017-09-25 07:00:00'), :'[', :')'
+          ),
+          left: range_value.new(
+            Time.parse('2017-09-25 01:00:00'),
+            Time.parse('2017-09-25 04:00:00'), :'[', :')'
+          ),
+          right: range_value.new(
+            Time.parse('2017-09-25 09:00:00'),
+            Time.parse('2017-09-25 12:00:00'), :'(', :')'
+          )
+        }
+      end
+
+      let(:ref_value) { '[2017-09-25 04:00:00, 2017-09-25 08:00:00]' }
+
+      it_behaves_like 'range type'
+    end
+
+    describe 'daterange' do
+      let(:db_type) { :daterange }
+
+      let(:values) do
+        {
+          containing: range_value.new(
+            Date.parse('2017-10-03'),
+            Date.parse('2017-10-09'), :'[', :']'
+          ),
+          contained: range_value.new(
+            Date.parse('2017-10-05'),
+            Date.parse('2017-10-07'), :'[', :')'
+          ),
+          left: range_value.new(
+            Date.parse('2017-10-01'),
+            Date.parse('2017-10-04'), :'[', :')'
+          ),
+          right: range_value.new(
+            Date.parse('2017-10-09'),
+            Date.parse('2017-10-12'), :'(', :')'
+          )
+        }
+      end
+
+      let(:ref_value) { '[2017-10-04, 2017-10-08]' }
+
+      it_behaves_like 'range type'
+    end
+  end
+end
