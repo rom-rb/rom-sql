4fb821449c8ec41cc0e1d88585f02a2a1ad95742
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-11-02 09:46:07 +0900

    Add an explicit spec for #by_pk view
    Closes #210

---
+++ spec/integration/relation/default_views_spec.rb
@@ -0,0 +1,31 @@
+RSpec.describe 'Relation / Default views' do
+  include_context 'database setup'
+
+  with_adapters do
+    describe '#by_pk' do
+      subject(:users) { relations[:users] }
+
+      context 'when dataset is overridden' do
+        before do
+          conn.create_table(:users) do
+            primary_key :id
+            column :name, String
+            column :email, String
+          end
+
+          conf.relation(:users) do
+            dataset { select(:name) }
+            schema(infer: true)
+          end
+        end
+
+        it 'restricts a relation by its primary key' do
+          users.insert(name: 'Jane')
+          pk = users.insert(name: 'Joe')
+
+          expect(users.by_pk(pk).one).to eql(name: 'Joe')
+        end
+      end
+    end
+  end
+end
