3558c47ac423793c31f3598d2952fa29a5db6939
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-11-10 11:42:22 +0100

    Workaround memory leak in jruby when rspec spy is used

--- spec/unit/relation/instrument_spec.rb
+++ spec/unit/relation/instrument_spec.rb
@@ -1,43 +1,67 @@
 RSpec.describe ROM::SQL::Relation, '#instrument', :sqlite do
-  include_context 'users and tasks'
+  include_context 'database setup'
 
   subject(:relation) do
     relations[:users]
   end
 
   let(:notifications) do
-    spy(:notifications)
+    Class.new do
+      attr_reader :logs
+
+      def initialize
+        @logs = []
+      end
+
+      def instrument(*args, &block)
+        logs << args
+        block.call
+      end
+    end.new
   end
 
   before do
+    conn.create_table :users do
+      primary_key :id
+      column :name, String
+    end
+
     conf.plugin(:sql, relations: :instrumentation) do |p|
       p.notifications = notifications
     end
+
+    conf.relation(:users) do
+      schema(infer: true)
+    end
+  end
+
+  after do
+    conn.drop_table(:users)
   end
 
   it 'instruments relation materialization' do
-    users.to_a
+    relation.to_a
 
-    expect(notifications).
-      to have_received(:instrument).with(:sql, name: :sqlite, query: users.dataset.sql)
+    expect(notifications.logs).
+      to include([:sql, name: :sqlite, query: relation.dataset.sql])
   end
 
   it 'instruments methods that return a single tuple' do
-    users.first
+    relation.first
 
-    expect(notifications).
-      to have_received(:instrument).with(:sql, name: :sqlite, query: users.limit(1).dataset.sql)
+    expect(notifications.logs).
+      to include([:sql, name: :sqlite, query: relation.limit(1).dataset.sql])
 
-    users.last
+    relation.last
 
-    expect(notifications).
-      to have_received(:instrument).with(:sql, name: :sqlite, query: users.reverse.limit(1).dataset.sql)
+    expect(notifications.logs).
+      to include([:sql, name: :sqlite, query: relation.reverse.limit(1).dataset.sql])
   end
 
   it 'instruments aggregation methods' do
-    users.count
+    relation.count
 
-    expect(notifications).
-      to have_received(:instrument).with(:sql, name: :sqlite, query: "SELECT count(*) AS 'count' FROM `users` LIMIT 1")
+    expect(notifications.logs).
+      to include([:sql, name: :sqlite, query: "SELECT count(*) AS 'count' FROM `users` LIMIT 1"])
   end
 end
