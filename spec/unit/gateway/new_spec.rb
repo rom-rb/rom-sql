85b75c54ca50e8cd77ebcc52b0773f5e04fddc88
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-11-17 11:02:11 +0100

    Skip spec on jruby

--- spec/unit/gateway/new_spec.rb
+++ spec/unit/gateway/new_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe ROM::SQL::Gateway, '#initialize' do
     end
 
     it 'establishes connection' do
+      skip 'it is not supported by jruby' if jruby?
       expect(gateway.connection).to be_instance_of(Sequel::SQLite::Database)
     end
   end
