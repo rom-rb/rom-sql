e9939ef1a0b14e1946f5e6c8cb78c423367869ad
Nikita Shilnikov <fg@flashgordon.ru>
2019-10-06 17:33:46 +0300

    Make Relation#nullify a relation plugin (closes #337)

--- lib/rom/sql/plugins.rb
+++ lib/rom/sql/plugins.rb
@@ -2,10 +2,12 @@ require 'rom/plugins/relation/sql/instrumentation'
 require 'rom/plugins/relation/sql/auto_restrictions'
 
 require 'rom/sql/plugin/associates'
+require 'rom/sql/plugin/nullify'
 require 'rom/sql/plugin/pagination'
 
 ROM.plugins do
   adapter :sql do
+    register :nullify, ROM::SQL::Plugin::Nullify, type: :relation
     register :pagination, ROM::SQL::Plugin::Pagination, type: :relation
     register :associates, ROM::SQL::Plugin::Associates, type: :command
   end
---
+++ spec/unit/plugin/nullify_spec.rb
@@ -0,0 +1,19 @@
+require 'rom/sql/plugin/nullify'
+
+RSpec.describe ROM::Relation, '#nullify' do
+  include_context 'users'
+
+  before do
+    conf.relation(:users) do
+      use :nullify
+    end
+  end
+
+  with_adapters do
+    it 'nullifies a relation which has records' do
+      # pending 'not working on JRuby' if defined?(JRUBY_VERSION)
+      expect(users.to_a).not_to be_empty
+      expect(users.nullify.to_a).to be_empty
+    end
+  end
+end
