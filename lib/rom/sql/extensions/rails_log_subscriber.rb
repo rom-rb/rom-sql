62867221448f657519cd4526a725d81e9dfed697
Nikita Shilnikov <fg@flashgordon.ru>
2024-07-01 18:40:27 +0200

    Fix warning issued by AS log subscriber (fix #426, #427)
    Co-Authored-By: Ben D'Angelo <674090+bendangelo@users.noreply.github.com>

--- lib/rom/sql/extensions/rails_log_subscriber.rb
+++ lib/rom/sql/extensions/rails_log_subscriber.rb
@@ -5,6 +5,21 @@ require 'active_support/log_subscriber'
 module ROM
   module SQL
     class RailsLogSubscriber < ActiveSupport::LogSubscriber
+      as_version =
+        begin
+          require "active_support/gem_version"
+          ActiveSupport.gem_version
+        rescue LoadError
+          nil
+        end
+
+      COLOR_OPTION =
+        if as_version && as_version >= Gem::Version.new("7.2")
+          {color: true}
+        else
+          true
+        end
+
       def sql(event)
         return unless logger.debug?
 
@@ -15,10 +30,10 @@ module ROM
         binds = payload[:binds].to_a.inspect if payload[:binds]
 
         if odd?
-          name = color(name, :cyan, true)
-          sql  = color(sql, nil, true)
+          name = color(name, :cyan, COLOR_OPTION)
+          sql  = color(sql, nil, COLOR_OPTION)
         else
-          name = color(name, :magenta, true)
+          name = color(name, :magenta, COLOR_OPTION)
         end
 
         debug "  #{name}  #{sql}  #{binds}"
